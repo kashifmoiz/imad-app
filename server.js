f686f5b2cf8b0a42d98ee396eca9aa59e8d272e5
kashifmoiz <31029341+kashifmoiz@users.noreply.github.com>
2017-08-21 20:31:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article%20one', function (req, res) {
+app.get('/article-one', function (req, res) {
     res.send('Article one requested and will be served here');
 });
 app.get('/article%20two', function (req, res) {
