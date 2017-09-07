4c4f59f9964285bf5c339251d6bdda805fe79fb4
kashifmoiz <31029341+kashifmoiz@users.noreply.github.com>
2017-09-07 12:05:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ content: `
                             This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
                             </p>`
     };
+    
     function createTemplate(data) {
         var title = data.title;
         var date = data.date;
